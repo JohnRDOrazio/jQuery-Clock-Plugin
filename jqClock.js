b1180341e81a570c9a59a5292cb5e8f439399ce9
John R. D'Orazio <donjohn.fmmi@gmail.com>
2010-12-10 12:49:15 -0800

    completely revamped from head to toe, now substracts difference from client time instead of adding 1000 milliseconds every 1000 milliseconds

--- jqClock.js
+++ jqClock.js
@@ -29,6 +29,8 @@
 (function($, undefined) {
 
 $.clock = { version: "1.0.0", locale: {} }
+
+t = new Array();
   
 $.fn.clock = function(options) {
   var locale = {
@@ -61,68 +63,71 @@ $.fn.clock = function(options) {
   return this.each(function(){
     $.extend(locale,$.clock.locale);
     options = options || {};  
-    if(options=="destroy"){ $(this).updateClock($(this),"destroy"); }
-    if(options.timestamp!=undefined){
-      mytimestamp = options.timestamp;
+    options.timestamp = options.timestamp || "systime";
+    systimestamp = new Date();
+    systimestamp = systimestamp.getTime();
+    options.sysdiff = 0;
+    if(options.timestamp!="systime"){
       mytimestamp = new Date(options.timestamp);
-    }
-    else{ 
-      options.timestamp = new Date();
-      mytimestamp = options.timestamp;
-      options.timestamp = options.timestamp.getTime();
+      options.sysdiff = options.timestamp - systimestamp;
     }
     options.langSet = options.langSet || "en";
     options.format = options.format || ((options.langSet!="en") ? "24" : "12");
     options.calendar = options.calendar || "true";
 
-    options.timestamp += 1000;
+    if (!$(this).hasClass("jqclock")){$(this).addClass("jqclock");}
+
     var addleadingzero = function(i){
       if (i<10){i="0" + i;}
       return i;    
     },
-    h=mytimestamp.getHours(),
-    m=mytimestamp.getMinutes(),
-    s=mytimestamp.getSeconds(),
-    dy=mytimestamp.getDay(),
-    dt=mytimestamp.getDate(),
-    mo=mytimestamp.getMonth(),
-    y=mytimestamp.getFullYear(),
-    ap="",
-    calend="";
-  
-    if(options.format=="12"){
-      ap=" AM";
-      if (h > 11) { ap = " PM"; }
-      if (h > 12) { h = h - 12; }
-      if (h == 0) { h = 12; }
-    }
-
-    // add a zero in front of numbers 0-9
-    h=addleadingzero(h);
-    m=addleadingzero(m);
-    s=addleadingzero(s);
-
-    if(options.calendar!="false") {
-      if (options.langSet=="en") {
-        calend = "<span class='clockdate'>"+locale[options.langSet].weekdays[dy]+', '+locale[options.langSet].months[mo]+' '+dt+', '+y+"</span>";
-      }
+    updateClock = function(el,myoptions) {
+      var el_id = $(el).attr("id");
+      if(myoptions=="destroy"){ clearTimeout(t[el_id]); }
       else {
-        calend = "<span class='clockdate'>"+locale[options.langSet].weekdays[dy]+', '+dt+' '+locale[options.langSet].months[mo]+' '+y+"</span>";
+        mytimestamp = new Date();
+        mytimestamp = mytimestamp.getTime();
+        mytimestamp = mytimestamp - myoptions.sysdiff;
+        mytimestamp = new Date(mytimestamp);
+        var h=mytimestamp.getHours(),
+        m=mytimestamp.getMinutes(),
+        s=mytimestamp.getSeconds(),
+        dy=mytimestamp.getDay(),
+        dt=mytimestamp.getDate(),
+        mo=mytimestamp.getMonth(),
+        y=mytimestamp.getFullYear(),
+        ap="",
+        calend="";
+
+        if(myoptions.format=="12"){
+          ap=" AM";
+          if (h > 11) { ap = " PM"; }
+          if (h > 12) { h = h - 12; }
+          if (h == 0) { h = 12; }
+        }
+
+        // add a zero in front of numbers 0-9
+        h=addleadingzero(h);
+        m=addleadingzero(m);
+        s=addleadingzero(s);
+
+        if(myoptions.calendar!="false") {
+          if (myoptions.langSet=="en") {
+            calend = "<span class='clockdate'>"+locale[myoptions.langSet].weekdays[dy]+', '+locale[myoptions.langSet].months[mo]+' '+dt+', '+y+"</span>";
+          }
+          else {
+            calend = "<span class='clockdate'>"+locale[myoptions.langSet].weekdays[dy]+', '+dt+' '+locale[myoptions.langSet].months[mo]+' '+y+"</span>";
+          }
+        }
+        $(el).html(calend+"<span class='clocktime'>"+h+":"+m+":"+s+ap+"</span>");
+        t[el_id] = setTimeout(function() { updateClock( $(el),myoptions ) }, 1000);
       }
+
     }
-    if ( !$(this).hasClass("jqclock") ) {
-      $(this).addClass("jqclock");
-    }
-    $(this).html(calend+"<span class='clocktime'>"+h+":"+m+":"+s+ap+"</span>");
-    $(this).updateClock($(this),{"calendar":options.calendar,"timestamp":options.timestamp,"langSet":options.langSet,"format":options.format});
+      
+    updateClock($(this),options);
   });
 }
-  t = new Array();
-  $.fn.updateClock = function(el,myoptions) {
-    var el_id = $(el).attr("id");
-    if(myoptions=="destroy"){ clearTimeout(t[el_id]); }
-    else { t[el_id] = setTimeout(function() {$(el).clock(myoptions)}, 1000); }
-    }
 
   return this;
 
