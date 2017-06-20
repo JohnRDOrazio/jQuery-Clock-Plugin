cda40a71c6183cc52225c6f961f6361c3d24b8e3
John R. D'Orazio <john.dorazio@cappellaniauniroma3.org>
2017-06-20 21:47:43 +0200

    add info in comment about performance.now

--- jqClock.js
+++ jqClock.js
@@ -80,6 +80,17 @@ if (!String.prototype.padStart) {
 }
 //END STRING.PROTOTYPE.PADSTART
 
+/* Might be able to use performance.now: 
+ * https://developer.mozilla.org/en-US/docs/Web/API/Performance/now
+ * Unlike other timing data available to JavaScript (for example Date.now), 
+ * the timestamps returned by Performance.now() are not limited to one-millisecond resolution. 
+ * Instead, they represent times as floating-point numbers with up to microsecond precision.
+ * Also unlike Date.now(), the values returned by Performance.now() always increase at a constant rate, 
+ * independent of the system clock (which might be adjusted manually or skewed by software like NTP). 
+ * Otherwise, performance.timing.navigationStart + performance.now() will be approximately equal to Date.now(). <<<<<<<<
+ * See also https://developers.google.com/web/updates/2012/08/When-milliseconds-are-not-enough-performance-now
+ */
+
 //BEGIN JQUERY CLOCK PLUGIN
 (function($, undefined) {
 
